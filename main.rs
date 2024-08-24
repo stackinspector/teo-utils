014df3c23218aae1c6ab41a683b51569f47324eb
stackinspector <stackinspector@126.com>
2024-08-24 18:34:37 +0800

    range

--- main.rs
+++ main.rs
@@ -51,53 +51,67 @@ fn main() {
     let secret_id = std::fs::read_to_string("sid").unwrap();
     let secret_key = std::fs::read_to_string("sk").unwrap();
     let zone_id = std::fs::read_to_string("zid").unwrap();
+    let access = Access { secret_id, secret_key };
 
     use std::io::{Read, Write};
 
-    let max_limit = 300;
-    let payload = DownloadL7Logs {
-        start_time: "2024-07-17T00:00:00+08:00".to_owned(),
-        end_time: "2024-07-17T23:59:00+08:00".to_owned(),
-        zone_ids: vec![zone_id],
-        domains: None,
-        limit: max_limit,
-        offset: 0,
-    };
-    let access = Access { secret_id, secret_key };
-    let req = build_request(&payload, now(), &access);
-    println!("{:?}", req);
-    let res = ureq(req);
-    let res: ResponseWrapper<DownloadL7LogsRes> = serde_json::from_reader(res).unwrap();
-    let res = res.response;
-    assert!(res.total_count <= max_limit); // empirical & adhoc
+    use chrono::{TimeZone, Datelike};
+    let time_zone = chrono::FixedOffset::east_opt(8 * 3600).unwrap();
+    let start_date_naive = chrono::NaiveDate::from_ymd_opt(2024, 7, 17).unwrap();
+    #[allow(deprecated)]
+    let mut date = time_zone.from_local_date(&start_date_naive).unwrap();
 
-    let mut dst_buf = Vec::with_capacity(16777216); // empirical
+    loop {
+        let max_limit = 300;
+        let payload = DownloadL7Logs {
+            start_time: date.and_hms_opt(0, 0, 0).unwrap().to_rfc3339(),
+            end_time: date.and_hms_opt(23, 59, 0).unwrap().to_rfc3339(),
+            zone_ids: vec![zone_id.clone()],
+            domains: None,
+            limit: max_limit,
+            offset: 0,
+        };
+        let req = build_request(&payload, now(), &access);
+        println!("{:?}", req);
+        let res = ureq(req);
+        let res: ResponseWrapper<DownloadL7LogsRes> = serde_json::from_reader(res).unwrap();
+        let res = res.response;
+        assert!(res.total_count <= max_limit); // empirical & adhoc
 
-    for item in res.data {
-        let L7OfflineLog { domain, area, log_packet_name, url, log_time, log_start_time, log_end_time, size } = item;
-        let gz_handle = ureq::get(&url).call().unwrap().into_reader();
+        let mut dst_buf = Vec::with_capacity(16777216); // empirical
 
-        let mut gz_reader = flate2::read::MultiGzDecoder::new(gz_handle);
-        let mut uncompressed_buf = String::new();
-        gz_reader.read_to_string(&mut uncompressed_buf).unwrap();
-        let uncompressed_size = uncompressed_buf.len().try_into().unwrap();
+        for item in res.data {
+            let L7OfflineLog { domain, area, log_packet_name, url, log_time, log_start_time, log_end_time, size } = item;
+            let gz_handle = ureq::get(&url).call().unwrap().into_reader();
 
-        let gz_header = gz_reader.header().unwrap();
-        assert!(gz_header.extra().is_none()); // .is_some_and(|v| v.is_empty())
-        assert!(gz_header.comment().is_none());
-        let gz_filename = String::from_utf8(gz_header.filename().unwrap().to_owned()).unwrap();
-        let gz_mtime = gz_header.mtime();
+            let mut gz_reader = flate2::read::MultiGzDecoder::new(gz_handle);
+            let mut uncompressed_buf = String::new();
+            gz_reader.read_to_string(&mut uncompressed_buf).unwrap();
+            let uncompressed_size = uncompressed_buf.len().try_into().unwrap();
 
-        let url_without_query = remove_url_query(&url);
-        let info = LogInfo { domain, area, log_packet_name, url_without_query, log_time, log_start_time, log_end_time, size, gz_filename, gz_mtime, uncompressed_size };
-        let info = serde_json::to_string(&info).unwrap();
-        println!("{}", info);
-        dst_buf.extend_from_slice(info.as_bytes());
-        dst_buf.extend_from_slice("\n".as_bytes());
-        dst_buf.extend_from_slice(uncompressed_buf.as_bytes());
+            let gz_header = gz_reader.header().unwrap();
+            assert!(gz_header.extra().is_none()); // .is_some_and(|v| v.is_empty())
+            assert!(gz_header.comment().is_none());
+            let gz_filename = String::from_utf8(gz_header.filename().unwrap().to_owned()).unwrap();
+            let gz_mtime = gz_header.mtime();
+
+            let url_without_query = remove_url_query(&url);
+            let info = LogInfo { domain, area, log_packet_name, url_without_query, log_time, log_start_time, log_end_time, size, gz_filename, gz_mtime, uncompressed_size };
+            let info = serde_json::to_string(&info).unwrap();
+            println!("{}", info);
+            dst_buf.extend_from_slice(info.as_bytes());
+            dst_buf.extend_from_slice("\n".as_bytes());
+            dst_buf.extend_from_slice(uncompressed_buf.as_bytes());
+        }
+
+        let dst_filename = format!("{}-{zone_id}.xz", date.format("%Y%m%d"));
+        let dst_file = std::fs::OpenOptions::new().create_new(true).write(true).open(dst_filename).unwrap();
+        let mut xz_handle = xz2::write::XzEncoder::new(dst_file, 9);
+        xz_handle.write_all(dst_buf.as_slice()).unwrap();
+
+        date = date.checked_add_signed(chrono::TimeDelta::days(1)).unwrap();
+        if date.month() == 8 && date.day() == 23 {
+            break;
+        }
     }
-
-    let dst_file = std::fs::OpenOptions::new().create_new(true).write(true).open("dst").unwrap();
-    let mut xz_handle = xz2::write::XzEncoder::new(dst_file, 9);
-    xz_handle.write_all(dst_buf.as_slice()).unwrap();
 }
